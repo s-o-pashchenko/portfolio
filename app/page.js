1d9f320e7b6686fa321fc1c0b866504c0111f7af
Serhii Pashchenko <s.o.pashchneko@gmail.com>
2024-04-14 11:18:25 -0500

    apostrophe

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>Serhii Pashchenko's portfolio is still under development</p>
+      <p>Serhii Pashchenko&apos;s portfolio is still under development</p>
     </main>
   );
 }
